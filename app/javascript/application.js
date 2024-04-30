27366398a1b4c9feacb0e3601de3473bbb2817f2
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-04-30 18:02:07 +0900

    hello_controller.jsファイル修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 
 document.addEventListener('turbo:load', () => {
   const addTaskForm = document.getElementById('add-task-form');
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["complete"];
+
   connect() {
-    this.element.textContent = "Hello World!"
+    this.element.textContent = "Hello, TaskMaster2!";
+  }
+
+  toggleComplete(event) {
+    event.preventDefault();
+    const taskId = this.data.get("id"); // HTMLからdata-idを取得
+    // ここにタスクの完了状態を更新するAPI呼び出しを記述するか、ローカルの状態を変更するロジックを記述
+    console.log(`Toggling complete status for task ${taskId}`);
+    this.completeTarget.classList.toggle("is-complete"); // CSSクラスをトグル
   }
 }
