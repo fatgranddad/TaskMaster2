269a4765e96e2d69c2965b82d61a30bff1c629aa
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-05-02 01:19:33 +0900

    @hotwired/stimulus-loading削除
    公式に存在しない@hotwired/stimulus-loading に関する行を importmap.rb ファイルから削除

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,5 +3,4 @@
 pin "application"
 pin "@hotwired/turbo-rails", to: "https://cdn.skypack.dev/@hotwired/turbo@7.1.0/dist/turbo.min.js"
 pin "@hotwired/stimulus", to: "https://cdn.skypack.dev/@hotwired/stimulus@3.0.0/dist/stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "https://cdn.skypack.dev/@hotwired/stimulus-loading@1.0.0/dist/stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
