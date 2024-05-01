b4a48930405ee539654170adec3e341366b38f95
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-05-01 13:53:50 +0900

    コントローラーを設定（new create）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,20 @@
 class TasksController < ApplicationController
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクが追加されました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :details, :due_date)
   end
 end
