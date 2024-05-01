4b4d5b75216100ffbbcb34912f0cd79525b62446
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-05-01 15:03:28 +0900

    コントローラー設定（edit update）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,19 @@ class TasksController < ApplicationController
     @tasks = Task.all  # すべてのタスクを取得
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'タスクが更新されました。'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def task_params
