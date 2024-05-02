16938aca5ac58b36b6ae4a1b0e4b18724ce9dd67
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-05-02 15:31:48 +0900

    新しいコントローラgreeting_controller.jsの作成

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
 import { Application } from "@hotwired/stimulus"
+import GreetingController from "./controllers/greeting_controller";  // 新しいコントローラをインポート
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = true  // 開発中はデバッグ情報を有効にする
-window.Stimulus   = application
+window.Stimulus = application
+
+// 既存のコントローラー登録後に新しいコントローラを登録
+application.register("greeting", GreetingController);  // 新しいコントローラを登録
 
 export { application }
