712349a4848d7f3983a94e22734adea13ced53d5
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-05-02 18:54:38 +0900

    Parcelを最新版に更新

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,5 +2,5 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "https://cdn.skypack.dev/@hotwired/turbo@7.1.0/dist/turbo.min.js"
-pin "@hotwired/stimulus", to: "https://cdn.skypack.dev/@hotwired/stimulus@3.2.2/dist/stimulus.min.js"
+# pin "@hotwired/stimulus", to: "https://cdn.skypack.dev/@hotwired/stimulus@3.2.2/dist/stimulus.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
