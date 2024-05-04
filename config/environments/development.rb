fa97850ce174ee483bc097896e1ee6017a2d3c0e
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-05-04 15:19:12 +0900

    development.rb ファイル更新
    # 静的ファイルサーバーを有効にする
    config.public_file_server.enabled = true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # 静的ファイルサーバーを有効にする
+  config.public_file_server.enabled = true
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
